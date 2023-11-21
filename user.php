a2f9f097f3be37a160ff8e5fdc635217ab29e40a
Shumona5 <shumonashikha@gmail.com>
2023-11-21 21:33:36 +0600

    first commit

---
+++ user.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Crud Operation </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+    <div class="container" my-5>
+
+    <form>
+  <div class="mb-3">
+    <label  class="form-label"> Name </label>
+    <input type="text" class="form-control" name="name" placeholder="Enter Your Name" autocomplete="off">
+  </div>
+  <div class="mb-3">
+    <label  class="form-label"> Email </label>
+    <input type="email" class="form-control" name="email" placeholder="Enter Your Email" >
+  </div>
+  <div class="mb-3">
+    <label  class="form-label"> Phone </label>
+    <input type="number" class="form-control" name="phone" placeholder="Enter Your Phone Number" >
+  </div>
+  <div class="mb-3">
+    <label  class="form-label"> Password </label>
+    <input type="text" class="form-control" name="password" placeholder="Password" >
+  </div>
+  
+ 
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+
+    </div>
+    
+  </body>
+</html>
